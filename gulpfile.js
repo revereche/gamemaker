03d64e20159a00506c9aeaded7d8ffb2bad3e1cb
Margret Storm <i.am.ozark@gmail.com>
2016-11-07 22:53:40 -0800

    Incorporated Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass')
 var electron = require('electron-connect').server.create();
 
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
 
 gulp.task('default', function() {
-  // Start browser process
-electron.start();
+    // Start browser process
+    electron.start();
 
-// Restart browser process
-gulp.watch('main.js', electron.restart);
+    // Restart browser process
+    gulp.watch('main.js', electron.restart);
 
-// Reload renderer process
-gulp.watch(['index.html', './js/test.js'], electron.reload);
+    // Reload renderer process
+    gulp.watch(['index.html',
+        './js/test.js',
+        './css/style.css'
+    ], electron.reload);
+
+    // Watching for Gulp-Sass
+    gulp.watch('sass/**/*.scss', ['styles']);
 
 });
