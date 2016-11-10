aa176255548dbe1a686299bf98df8d54b9d45875
Margret Storm <i.am.ozark@gmail.com>
2016-11-09 23:08:42 -0800

    jQuery animation working

--- js/test.js
+++ js/test.js
@@ -1,7 +1,14 @@
-$(function(){
-  var again = require('./js/testagain.js');
+$(document).ready(function() {
+    var again = require('./js/testagain.js');
 
-  $("#stage").append("<div id='bg'></div>");
-  $("#stage").append("<div id='sprite'></div>");
+    $("#stage").append("<div id='bg'></div>");
+    $("#stage").append("<div id='sprite'></div>");
+
+    $("#btn1").click(function() {
+        $("#bg").animate({
+          width: "300px",
+          left: "10%"
+        });
+    });
 
 });
